98ec1b16bf5d010434c0dc4f56227c8a5e7d66d3
illenko <kostiaillenko@gmail.com>
2022-08-28 15:07:47 +0300

    Minor fixes.

--- client/src/main/kotlin/com/illenko/client/config/SecurityConfig.kt
+++ client/src/main/kotlin/com/illenko/client/config/SecurityConfig.kt
@@ -3,10 +3,10 @@ package com.illenko.client.config
 import org.springframework.context.annotation.Bean
 import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
 
-@EnableWebFluxSecurity
+@EnableWebSecurity
 class SecurityConfig {
 
     @Bean
