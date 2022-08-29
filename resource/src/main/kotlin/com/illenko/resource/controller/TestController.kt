5bf2bddfa191aeb3bd082447c0a102676c35aca9
illenko <kostiaillenko@gmail.com>
2022-08-29 09:02:13 +0300

    Minor changes.

--- resource/src/main/kotlin/com/illenko/resource/controller/TestController.kt
+++ resource/src/main/kotlin/com/illenko/resource/controller/TestController.kt
@@ -1,14 +1,25 @@
 package com.illenko.resource.controller
 
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class TestController {
 
+
     @GetMapping("/test")
     fun test(): Array<String> {
-        return arrayOf("Test 1", "Test 2", "Test 3")
+        val authentication = SecurityContextHolder.getContext().authentication
+
+        return arrayOf(
+            authentication.name,
+            authentication.authorities.toString(),
+            authentication.details.toString(),
+            (authentication.principal as Jwt).issuer.toString(),
+            (authentication.principal as Jwt).issuedAt.toString()
+        )
     }
 
 }
